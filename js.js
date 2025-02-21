ccfc811777a4ea2de4c64adc5c28fa875cb669e1
lana <lanalezhava4@gmail.com>
2025-02-21 13:30:52 +0100

    dan

---
+++ js.js
@@ -0,0 +1,10 @@
+const openBtn = document.querySelector('.open-btn')
+// const closeBtn = document.querySelector('.close-btn')
+const navBar = document.querySelector('nav ')
+
+openBtn.addEventListener('click', () => {
+    navBar.classList.toggle('active')
+})
+// slider
+
+
